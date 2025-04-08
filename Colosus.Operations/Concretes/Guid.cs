c5bcb11c92d30250cc117eea135a44aef294f702
Yussefuynstein <Yussefuynstein@DESKTOP-5KJ4L46>
2025-04-08 04:56:33 +0300

    Url'de sorun çıkartığı için düzeltildi.

--- Colosus.Operations/Concretes/Guid.cs
+++ Colosus.Operations/Concretes/Guid.cs
@@ -10,8 +10,16 @@ namespace Colosus.Operations.Concretes
     public class Guid : IGuid
     {
 
-        private string _generate() => $"{System.Guid.NewGuid()}-{DateTime.Now.ToString().Replace(" ","")}";
+        private string _generate() => $"{System.Guid.NewGuid()}-{DateTime.Now.ToString()}";
         public string Generate(string StartInd) => $"{StartInd.ToString()}-{_generate()}";
-        public string Generate(string StartInd, string LastInd) => $"{Generate(StartInd.ToString())}-{LastInd.ToString()}";
+        public string Generate(string StartInd, string LastInd) => UrlRewrite($"{Generate(StartInd.ToString())}-{LastInd.ToString()}");
+        private string UrlRewrite(string data)
+        {
+            string returned = data;
+            char[] redChars = new char[] { '"', '/', ':', ' ', '#', '?', '&', '=', '%', '<', '>', '{', '}', '[', ']', '\\' };
+            foreach (var redChar in redChars)
+                returned = returned.Replace(redChar.ToString(), string.Empty);
+            return returned;
+        }
     }
 }
