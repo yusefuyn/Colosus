359706212c0fd5fb03680d6a674fe6b32fa6e166
User <User@DESKTOP-SPKGJQR>
2025-05-05 16:29:22 +0300

    Tablo nesnesi tasarladım

---
+++ Colosus/Client/Components/Table.razor.cs
@@ -0,0 +1,10 @@
+﻿namespace Colosus.Client.Blazor.Components
+{
+
+    public class TableColumn { 
+        public string VisibleName { get; set; }
+        public string PropertyName { get; set; }
+        public int Width { get; set; }
+        public bool IsVisible { get; set; }
+    }
+}
